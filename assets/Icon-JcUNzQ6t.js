946193c0957d8956c7f170661a2a5116df03dd7b
Jorge-R-V <alumno.656115@ies-azarquiel.es>
2026-02-19 17:07:42 +0100

    deploy

---
+++ assets/Icon-JcUNzQ6t.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CLR75Ajd.js";function i({name:r,className:e="",...t}){return s.jsx("svg",{className:e,...t,children:s.jsx("use",{xlinkHref:`/assets/sprite.svg#${r}`})})}export{i as I};
